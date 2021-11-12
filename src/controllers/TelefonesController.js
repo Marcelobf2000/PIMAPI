97c2ab20094d063d5bbe1358b4fd095576fff7a6
Marcelobf2000 <marcelobf00@hotmail.com>
2021-11-12 17:10:23 -0300

    Indexação da controller telefone

--- src/controllers/TelefonesController.js
+++ src/controllers/TelefonesController.js
@@ -4,38 +4,38 @@ const { index } = require("./PessoaController");
 
 module.exports = {
 
-    async index(req, res){
-        const {pessoa_id} = req.params;
+    async index(req, res) {
+        const { pessoa_id } = req.params;
 
         const pessoa = await Pessoa.findByPk(pessoa_id, {
-            include: {association: 'phone'} //aqui passa a associaçã que o sequelize deve executar - essa associação criamos na model
+            include: { association: 'phone' } //aqui passa a associaçã que o sequelize deve executar - essa associação criamos na model
         });
 
         return res.json(pessoa);
     },
 
-    async store(req, res){
-        try{
-         
-         const { pessoa_id} = req.params;
-         const {celuar, fixo} = req.body;
+    async store(req, res) {
+        try {
 
-         const pessoa = await Pessoa.findByPk(pessoa_id);
+            const { pessoa_id } = req.params;
+            const { celuar, fixo } = req.body;
 
-        if (!pessoa){
-            return res.status(400).json({error: 'person not found'});
+            const pessoa = await Pessoa.findByPk(pessoa_id);
+
+            if (!pessoa) {
+                return res.status(400).json({ error: 'person not found' });
+            }
+
+            const telefones = await Telefones.create({
+                celuar,
+                fixo,
+                pessoa_id
+            });
+
+            return res.json(telefones);
+
+        } catch (err) {
+            console.log(err);
         }
-
-        const telefones = await Telefones.create({
-            celuar,
-            fixo,
-            pessoa_id
-        });
-
-        return res.json(telefones);
-
-      }  catch(err){
-          console.log(err);
-      }
     }
 }
